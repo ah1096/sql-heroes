aac751db5c383b4429e440a00460e8f769ffbd43
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-11-03 21:24:31 +0000

    view_hero works; working on delete_hero

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,5 +1,14 @@
 import psycopg
 from psycopg import OperationalError
+from os import system, name
+from time import sleep
+
+#for clearing the terminal; see README for link to documentation:
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 def create_connection(db_name, db_user, db_password, db_host = "127.0.0.1", db_port = "5432"):
     connection = None
@@ -11,19 +20,19 @@ def create_connection(db_name, db_user, db_password, db_host = "127.0.0.1", db_p
             host=db_host,
             port=db_port,
         )
-        print("Connection to PostgreSQL DB successful")
+        #print("Connection to PostgreSQL DB successful")
     except OperationalError as e:
         print(f"The error '{e}' occurred")
     return connection
 
 
-def execute_query(query, params=None):
+def execute_query(query, params):
     connection = create_connection("postgres", "postgres", "postgres")
     cursor = connection.cursor()
     try:
         cursor.execute(query, params)
         connection.commit()
-        print("Query executed successfully")
+        #print("Query executed successfully")
         connection.close()
         return cursor
     except OSError as e:
@@ -36,53 +45,94 @@ def execute_query(query, params=None):
 #=============save this bit of code for the View Hero function
 def select_all():
     query = """
-        SELECT * from heroes
+        SELECT name from heroes
     """
-
     list_of_heroes = execute_query(query).fetchall()
-    print(list_of_heroes)
+    #print(f"""{list_of_heroes}""" + '\n')
     for record in list_of_heroes:
-        print(record[1])
+        print(f""" {record[0]}""")
 
-select_all()
 #=================================================================================
 
+#✅MENU==================================================================================
+def menu():
+    what_do = str(input(f"What would you like to do?" + '\n' +
+                            "[1] ADD hero profile" + '\n' +
+                            "[2] VIEW hero profile(s)" + '\n' +
+                            "[3] UPDATE hero profile(s)" + '\n' +
+                            "[4] DELETE hero profile(s)"))
+    if what_do == "1":
+        clear()
+        add_hero()
+    elif what_do == "2":
+        clear()
+        view_hero()
+    elif what_do == "3":
+        clear()
+        update_profile()
+    elif what_do == "4":
+        clear()
+        delete_hero()
 
 
 
-
-#CREATE===========================================================================
-def add_hero(add_name, add_about_me, add_biography):
-    add_name = input("What is your hero name? ")
-    add_about_me = input("Write a short tagline, fun fact about yourself, or your motto: ")
-    add_biography = input("What is your origin story? ")
+#✅CREATE===========================================================================
+def add_hero(add_name=None, add_about_me=None, add_biography=None):
+    add_name = input("Input hero name: ")
+    add_about_me = input("Input hero tagline, catchphrase, or motto: ")
+    add_biography = input("Input hero origin story/biography: ")
     #add_abilities = input("What powers do you have? ")
 
     print(add_name, add_about_me, add_biography)
     confirm_add = input(f"Would you like to add {add_name}'s profile? Enter [YES] to confirm, or [NO] to start over.")
-    if confirm_add == "YES" or "yes":
+    if confirm_add == "YES" or "yes" and len(confirm_add)==3:
         print(f"{add_name}'s profile has been created.")
         query = ("""
                     INSERT INTO heroes (name, about_me, biography)
-                    VALUES (add_name, add_about_me, add_biography)
+                    VALUES (%s, %s, %s)
                 """)
         execute_query(query,(add_name, add_about_me, add_biography))
-        #return to menu
+        sleep(3)
+        clear()
         menu()
-    elif confirm_add == "NO" or "no":
+    elif confirm_add == "NO" or "no" and len(confirm_add)==2:
+        print(f"Starting over.....")
+        sleep(3)
+        clear()
         add_hero()
         
 
-#READ=============================================================================
-def view_hero(hero_to_view):
-    hero_to_view = input("Input a specific hero name, or enter [A] to see all hero profiles")
+#✅READ=============================================================================
+def view_hero():
+    hero_to_view = input("Input a specific hero name, or enter [A] to see all hero profiles: ")
 
-    if hero_to_view == "A" or "a":
-        #show all profiles with bit of code @ top ?????????
-        print(f"{hero_to_view} -> show all; this text is here so Python doesn't get mad")
-    else:
-        #show only hero_to_view's profile ??????????
-        print(f"{hero_to_view} -> show profile; this text is here so Python doesn't get mad")
+    def show_one_profile(hero_name):
+            query = """
+                    SELECT * from heroes
+                    WHERE name = %s
+                    """
+
+            single_hero = execute_query(query, (hero_name,)).fetchone()
+            print(f"""{single_hero[1]}: 
+                    {single_hero[2]}
+                    {single_hero[3]} """)
+
+    if hero_to_view == "A" or "a" and len(hero_to_view)==1:
+        select_all()
+        return_to_menu = input("Type [M] to return to the menu, or enter a hero's name to view their profile: ")
+        if return_to_menu == "M" or "m" and len(return_to_menu)==1:
+            clear()
+            menu()
+        else:
+            show_one_profile(return_to_menu)
+            return_to_menu = input("Type [M] to return to the menu: ")
+            if return_to_menu == "M" or "m" and len(return_to_menu)==1:
+                clear()
+                menu()
+            
+            
+        
+
 
 
 #UPDATE===========================================================================
@@ -95,7 +145,7 @@ def update_profile(hero_to_update=None, updated_name=None, updated_about_me=None
         #do nothing??
         print(f"{updated_name}do nothing; this text is here so Python doesn't get mad")
     else:
-        query = (""" 
+        query = ("""
                     UPDATE heroes
                     SET name = %s
                     WHERE name = hero_to_update
@@ -146,54 +196,44 @@ def update_profile(hero_to_update=None, updated_name=None, updated_about_me=None
 
 
 #DELETE==========================================================================
-def delete_hero(hero_to_delete):
+def delete_hero(hero_to_delete=None):
     hero_to_delete = input('Type the name of the hero whose profile you wish to DELETE: ')
     pollice_verso = input(f'Are you sure you want to delete {hero_to_delete}? Enter [YES] or [NO]: ')
     
-    if pollice_verso == "YES":
+    if pollice_verso == "YES" or "yes" and len(pollice_verso)==3:
         print(f"{hero_to_delete} has been deleted")
         query = ("""
-                    DELETE FROM heroes WHERE name = hero_to_delete
+                    DELETE FROM heroes WHERE name = %s
                 """)
         execute_query(query,(hero_to_delete))
-        #return to menu -> figure out when done with menu function
-    elif pollice_verso == "NO":
+        menu()
+
+    elif pollice_verso == "NO" or "no" and len(pollice_verso)==2:
         cold_feet = input("Would you like to delete a different hero [DEL], or return to the menu [M]?")
         if cold_feet == "DEL":
-            delete(hero)
+            delete(hero_to_delete)
         elif cold_feet == "M":
-            #return to menu
-            print("return to menu; this text is here so Python doesn't get mad")
+            menu()
 
-#MENU============================================================================
+#LOGIN PAGE============================================================================
 
 user = input("Welcome to SuperBase. Log in as: ")
 password = input("Input password: ")
 #make it more fun 
     #add a password? wrong pass = booted
-    #SuperBase is a secret government database 
+    #SuperBase is a secret government database??
     #Barbara Gordon/Oracle's database (input "BarbieG", get welcomed as "oracle")
-    #villain's record of adversaries
+    #villain's record of adversaries??
 if user == "BarbieG" and password == "KYSS23":
+    clear()
     print("Welcome back, Oracle.")
+    menu()
 else:
     print(f"Hello, {user}. You're not supposed to be here." + '\n' +
                 "SECURITY PROTOCOLS INITIATED" + '\n' +
                 f"TARGET INTRUDER: {user}")
+    sleep(3)
+    clear()
 
-def menu():
-    what_do = str(input(f"What would you like to do?" + '\n' +
-                            "[1] ADD hero profile" + '\n' +
-                            "[2] VIEW hero profile(s)" + '\n' +
-                            "[3] UPDATE hero profile(s)" + '\n' +
-                            "[4] DELETE hero profile(s)"))
-    if what_do == "1":
-        add_hero()
-    elif what_do == "2":
-        view_hero()
-    elif what_do == "3":
-        update_profile()
-    elif what_do == "4":
-        delete_hero()
 
-menu()
+
