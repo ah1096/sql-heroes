ce39dc24e8192115ec1a1c8b11ed7d19803ba138
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-11-03 14:36:07 +0000

    created view hero function; transferred pseudocode

--- src/database/connection.py
+++ src/database/connection.py
@@ -30,7 +30,7 @@ def execute_query(query, params=None):
         print(f"The error '{e}' occurred or the hero name is already taken")
 
 
-#PUT FUNCTIONS AND WHATNOT VVV HERE VVV UNTIL JOSH FIGURES STUFF OUT, DO NOT EDIT ABOVE
+#=================PUT FUNCTIONS AND WHATNOT VVV HERE VVV UNTIL JOSH FIGURES STUFF OUT, DO NOT EDIT ABOVE=======
 
 
 #save this bit of code for the View Hero function
@@ -48,29 +48,44 @@ def execute_query(query, params=None):
 
 #add abilities to this function later!!!!!!!!!!!!
 def add_hero(add_name, add_about_me, add_biography):
-    add_name = input("What if your hero name? ")
+    add_name = input("What is your hero name? ")
     add_about_me = input("Write a short tagline, fun fact about yourself, or your motto: ")
     add_biography = input("What is your origin story? ")
     #add_abilities = input("What powers do you have? ")
+    print(add_name, add_about_me, add_biography)
+
 
     ("""
         INSERT INTO heroes (name, about_me, biography)
         VALUES (add_name, add_about_me, add_biography)
     """)
 
-def delete_hero(deleted_hero):
-    deleted_hero = input('Type the name of the hero whose profile you wish to DELETE: ')
-    ("""
-        DELETE FROM heroes WHERE name = deleted_hero
-    """)
+def view_hero(hero_to_view):
+    view_choice = input("Input a specific hero name, or enter [A] to see all hero profiles")
+
+
 
 #ask for help on this function
 def update_profile(hero_to_update, updated_name, updated_about_me, updated_biography):
     hero_to_update = input("Type the name of the hero whose profile you wish to UPDATE: ")
     updated_name = input(f"Change {hero_to_update}'s name to: ")
-    updated_about_me = input("Change this hero's about me to: ")
-    updated_biography = input("Change this hero's biography to: ")
+    updated_about_me = input(f"Change {hero_to_update}'s about me to: ")
+    updated_biography = input(f"Change {hero_to_update}'s biography to: ")
     ("""
         UPDATE heroes
         SET name = updated_name)
+    """)
+
+def delete_hero(hero_to_delete):
+    hero_to_delete = input('Type the name of the hero whose profile you wish to DELETE: ')
+    pollice_verso = str.upper(input(f'Are you sure you want to delete {hero_to_delete}? Enter [YES] or [NO] '))
+    
+    if pollice_verso == "YES":
+        print(f"{hero_to_delete} has been deleted")
+        ("""
+            DELETE FROM heroes WHERE name = hero_to_delete
+        """)
+    elif pollice_verso == "NO":
+        
+        
     
